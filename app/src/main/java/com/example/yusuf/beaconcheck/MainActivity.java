f873f054d275ea23561c93b0df4cddc277984d97
rudypikulik <rudypikulik@gmail.com>
2018-10-13 12:41:42 -0400

    Added local saving for friends and courses

--- app/src/main/java/com/example/yusuf/beaconcheck/MainActivity.java
+++ app/src/main/java/com/example/yusuf/beaconcheck/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.yusuf.beaconcheck;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout courseLayout = findViewById(R.id.classListContainer);
+        Button joinButton = findViewById(R.id.joinClassButton);
+        // joinButton.setOnClickListener(new View.OnClickListener());
+        Course[] courseList = getCourseList();
+        String[] friendList = getFriendData();
+        if(courseList.length == 0){
+            TextView noCourses = new TextView(this);
+            noCourses.setText("No courses yet!");
+            noCourses.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.FILL_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            courseLayout.addView(noCourses);
+        }
+        else{
+            for(Course course: courseList){
+                TextView courseText = new TextView(this);
+                courseText.setText(course.getName());
+                courseText.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.FILL_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+                courseLayout.addView(courseText);
+            }
+        }
     }
+
+    public Course[] getCourseList(){
+        SharedPreferences courses = getApplicationContext().getSharedPreferences("Courses", 0);
+        Map<String, Integer> courseMap = (Map<String, Integer>)courses.getAll();
+        Course[] ret = new Course[courseMap.size()];
+        Object[] courseKeys = courseMap.keySet().toArray();
+        for(int i = 0; i < courseKeys.length; i++) {
+            ret[i] = new Course((String)courseKeys[i], courseMap.get(courseKeys[i]));
+        }
+        return ret;
+    }
+
+    public String[] getFriendData(){
+        SharedPreferences friends = getApplicationContext().getSharedPreferences("Friends", 0);
+        return Arrays.copyOf(friends.getAll().keySet().toArray(), friends.getAll().keySet().toArray().length, String[].class);
+    }
+
 }
